0e6566be15b20e27370668072ed108927e86f2ba
mateoszym <mateoszym@gmail.com>
2017-07-05 17:58:06 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@ function getTriangleArea(a, h) {
 	if ((a <= 0) || (h <= 0)) {
 	return ('Nieprawidłowe dane');
 	} else {
-	return a * h / 2; }
+	return a * h / 2; 
+	}
 }
 console.log( getTriangleArea(10,6) );
 console.log( getTriangleArea(15,25) );
